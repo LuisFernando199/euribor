9e8ac4b6230b73071ce81c388bce4e0f5ccdb5db
luisfernando.martinez <luisfernando.martinez@viewnext.com>
2024-08-08 12:15:11 +0200

    Ejecutable

--- app.py
+++ app.py
@@ -3,28 +3,6 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 
-# # Hacemos una solicitud GET a la página web
-# url = "https://www.bde.es/webbe/es/estadisticas/temas/tipos-interes.html"
-# response = requests.get(url)
-# print(response.status_code)
-
-# # Parseamos el HTML con BeautifulSoup
-# soup = BeautifulSoup(response.text, 'html.parser')
-
-# # Encontramos el elemento que contiene el valor del Euribor a un año
-# euribor_element = soup.find('p', {'class': 'block-card-indicator__value'})
-
-# print(response.text)
-
-# if euribor_element:
-#     # Extraemos el valor del Euribor a un año
-#     euribor_value = euribor_element.text.strip()
-#     print("Euribor a un año:", euribor_value)
-# else:
-#     print("No se encontró el elemento que contiene el valor del Euribor a un año")
-
-
-
 # Inicializamos el navegador
 driver = webdriver.Chrome()  # Reemplaza con el navegador que desees
 
@@ -65,3 +43,6 @@ else:
 
 # Cerramos el navegador
 driver.quit()
+
+# Mantener la ventana abierta hasta que el usuario decida cerrarla
+input("Presiona Enter para salir...")
